b762ac7b0087c6045eb1c322f403f826bf3812c1
Hao Ho <37636748+haomap123456@users.noreply.github.com>
2018-09-21 09:23:32 -0500

    First commit

---
+++ Assignment1/Models/HouseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment1.Models
+{
+    public class HouseModel
+    {
+        public int Value { get; set; }
+        public int LotSize {get;set;}
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
